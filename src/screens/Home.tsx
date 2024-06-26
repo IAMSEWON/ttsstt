4f801ac31a2e7474b7d1d8ef31c95d1e1070513d
Sewon Kim <i3brooooo@gmail.com>
2024-06-27 00:47:02 +0900

    [UPDATED] - react-native-voice 마이크 테스트

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,9 +1,58 @@
-import { Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Button, Text, View } from 'react-native';
+import Voice from 'react-native-voice';
 
 const Home = () => {
+  const [isRecord, setIsRecord] = useState<boolean>(false);
+  const [text, setText] = useState<string>('');
+  const buttonLabel = isRecord ? 'Stop' : 'Start';
+  const voiceLabel = text ? text : isRecord ? 'Say something...' : 'press Start button';
+
+  const _onSpeechStart = () => {
+    console.log('onSpeechStart');
+    setText('');
+  };
+  const _onSpeechEnd = () => {
+    console.log('onSpeechEnd');
+  };
+  const _onSpeechResults = (event) => {
+    console.log('onSpeechResults');
+    setText(event.value[0]);
+  };
+  const _onSpeechError = (event) => {
+    console.log('_onSpeechError');
+    console.log(event.error);
+  };
+
+  const _onRecordVoice = () => {
+    if (isRecord) {
+      Voice.stop();
+    } else {
+      Voice.start('ko-KR');
+    }
+    setIsRecord(!isRecord);
+  };
+
+  useEffect(() => {
+    Voice.onSpeechStart = _onSpeechStart;
+    Voice.onSpeechEnd = _onSpeechEnd;
+    Voice.onSpeechResults = _onSpeechResults;
+    Voice.onSpeechError = _onSpeechError;
+
+    return () => {
+      Voice.destroy().then(Voice.removeAllListeners);
+    };
+  }, []);
   return (
-    <View>
-      <Text>Home</Text>
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <Text>{voiceLabel}</Text>
+      <Button onPress={_onRecordVoice} title={buttonLabel} />
     </View>
   );
 };
